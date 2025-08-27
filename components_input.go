d086b11691a0fe1c845cfa8073d990ef7d675ac7
Edwin <edwinsyarief@users.noreply.github.com>
2025-08-27 19:17:49 +0700

    Refactor input

--- components_input.go
+++ components_input.go
@@ -1,38 +1,106 @@
 package katsu2d
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
 
+// Action represents a game action (e.g., "move_up", "jump").
 type Action string
 
+// InputType distinguishes between different types of input devices.
+type InputType int
+
+const (
+	InputTypeKeyboard InputType = 0
+	InputTypeMouse    InputType = 1
+	InputTypeGamepad  InputType = 2
+)
+
+// InputCode represents a specific key, button, or axis on a device.
+type InputCode struct {
+	Type InputType
+	Code int
+}
+
+// Binding holds a primary input and optional modifiers for an action.
+type Binding struct {
+	Primary   InputCode
+	Modifiers []InputCode
+}
+
 type KeyConfig struct {
-	Key                  ebiten.Key
-	Modifiers            []ebiten.Key
-	GamepadButton        ebiten.GamepadButton
-	GamepadModifiers     []ebiten.GamepadButton
-	MouseButton          ebiten.MouseButton
-	MouseButtonModifiers []ebiten.MouseButton
+	Primary   any
+	Modifiers []any
 }
 
+// InputComponent stores all input bindings and their current state.
 type InputComponent struct {
-	bindings      map[Action][]KeyConfig
-	actionState   map[Action]bool
-	justPressed   map[Action]bool
-	justReleased  map[Action]bool
-	previousState map[Action]bool
+	bindings     map[Action][]Binding
+	justPressed  map[Action]bool
+	pressed      map[Action]bool
+	JustReleased map[Action]bool
 }
 
-func NewInputComponent(bindings map[Action][]KeyConfig) *InputComponent {
-	return &InputComponent{
-		bindings:      bindings,
-		actionState:   make(map[Action]bool),
-		justPressed:   make(map[Action]bool),
-		justReleased:  make(map[Action]bool),
-		previousState: make(map[Action]bool),
+// NewInputComponent creates and initializes a new InputComponent.
+func NewInputComponent(bindinds map[Action][]KeyConfig) *InputComponent {
+	res := &InputComponent{
+		bindings:     make(map[Action][]Binding),
+		justPressed:  make(map[Action]bool),
+		pressed:      make(map[Action]bool),
+		JustReleased: make(map[Action]bool),
+	}
+
+	if len(bindinds) > 0 {
+		res.BatchBind(bindinds)
+	}
+
+	return res
+}
+
+// toInputCode converts a generic Ebitengine input type to a standardized InputCode.
+func toInputCode(v any) InputCode {
+	switch code := v.(type) {
+	case ebiten.Key:
+		return InputCode{Type: InputTypeKeyboard, Code: int(code)}
+	case ebiten.MouseButton:
+		return InputCode{Type: InputTypeMouse, Code: int(code)}
+	case ebiten.GamepadButton:
+		return InputCode{Type: InputTypeGamepad, Code: int(code)}
+	default:
+		// This should be improved with more graceful error handling in a real game.
+		panic("unsupported input type")
+	}
+}
+
+// Bind adds a new binding for a specific action.
+func (self *InputComponent) Bind(action Action, primary any, modifiers ...any) {
+	primaryCode := toInputCode(primary)
+	mods := make([]InputCode, len(modifiers))
+	for i, m := range modifiers {
+		mods[i] = toInputCode(m)
+	}
+	binding := Binding{
+		Primary:   primaryCode,
+		Modifiers: mods,
+	}
+	self.bindings[action] = append(self.bindings[action], binding)
+}
+
+// BatchBind replaces all existing bindings with a new set.
+// This is useful for loading a saved or preset control scheme.
+func (self *InputComponent) BatchBind(bindings map[Action][]KeyConfig) {
+	for a, b := range bindings {
+		// Clear existing bindings for this action to avoid duplicates
+		self.bindings[a] = []Binding{}
+		for _, k := range b {
+			self.Bind(a, k.Primary, k.Modifiers...)
+		}
 	}
 }
 
 func (self *InputComponent) IsPressed(action Action) bool {
-	return self.actionState[action]
+	return self.pressed[action]
 }
 
 func (self *InputComponent) IsJustPressed(action Action) bool {
@@ -40,5 +108,46 @@ func (self *InputComponent) IsJustPressed(action Action) bool {
 }
 
 func (self *InputComponent) IsJustReleased(action Action) bool {
-	return self.justReleased[action]
+	return self.JustReleased[action]
+}
+
+// isPressed checks if the given InputCode is currently pressed.
+func isPressed(code InputCode) bool {
+	switch code.Type {
+	case InputTypeMouse:
+		return ebiten.IsMouseButtonPressed(ebiten.MouseButton(code.Code))
+	case InputTypeKeyboard:
+		return ebiten.IsKeyPressed(ebiten.Key(code.Code))
+	case InputTypeGamepad:
+		// Note: This only checks for gamepad 0. You might want to extend this
+		// to handle multiple gamepads.
+		return ebiten.IsGamepadButtonPressed(0, ebiten.GamepadButton(code.Code))
+	}
+	return false
+}
+
+// isJustPressed checks if the given InputCode was pressed in the current frame.
+func isJustPressed(code InputCode) bool {
+	switch code.Type {
+	case InputTypeMouse:
+		return inpututil.IsMouseButtonJustPressed(ebiten.MouseButton(code.Code))
+	case InputTypeKeyboard:
+		return inpututil.IsKeyJustPressed(ebiten.Key(code.Code))
+	case InputTypeGamepad:
+		return inpututil.IsGamepadButtonJustPressed(0, ebiten.GamepadButton(code.Code))
+	}
+	return false
+}
+
+// isJustReleased checks if the given InputCode was released in the current frame.
+func isJustReleased(code InputCode) bool {
+	switch code.Type {
+	case InputTypeMouse:
+		return inpututil.IsMouseButtonJustReleased(ebiten.MouseButton(code.Code))
+	case InputTypeKeyboard:
+		return inpututil.IsKeyJustReleased(ebiten.Key(code.Code))
+	case InputTypeGamepad:
+		return inpututil.IsGamepadButtonJustReleased(0, ebiten.GamepadButton(code.Code))
+	}
+	return false
 }
--- systems_input.go
+++ systems_input.go
@@ -1,108 +1,46 @@
 package katsu2d
 
-import "github.com/hajimehoshi/ebiten/v2"
-
-// InputSystem is an UpdateSystem that handles all game input.
+// InputSystem is responsible for updating the state of all InputComponents.
 type InputSystem struct{}
 
-// NewInputSystem creates a new input system
+// NewInputSystem creates and initializes an InputSystem.
 func NewInputSystem() *InputSystem {
 	return &InputSystem{}
 }
 
-// Update implements the UpdateSystem interface. It polls the keyboard
-// and gamepad and updates the internal state of all actions. This should be run
-// once per game tick.
-func (self *InputSystem) Update(world *World, _ float64) {
+// Update processes all input components in the world.
+func (s *InputSystem) Update(world *World, delta float64) {
+	// First, reset all input states from the previous frame.
 	entities := world.Query(CTInput)
 	for _, e := range entities {
-		comp, _ := world.GetComponent(e, CTInput)
-		inputComp := comp.(*InputComponent)
-
-		// First, copy the current state to the previous state.
-		for action, isPressed := range inputComp.actionState {
-			inputComp.previousState[action] = isPressed
+		compAny, _ := world.GetComponent(e, CTInput)
+		comp := compAny.(*InputComponent)
+		for action := range comp.bindings {
+			comp.justPressed[action] = false
+			comp.pressed[action] = false
+			comp.JustReleased[action] = false
 		}
-
-		// Then, clear the current state to be re-evaluated.
-		for action := range inputComp.actionState {
-			inputComp.actionState[action] = false
-		}
-
-		// Iterate through all defined actions and their bindings to check for key presses.
-		for action, configs := range inputComp.bindings {
-			for _, config := range configs {
-				isPressed := false
-
-				// Check for keyboard input if a key is defined.
-				if config.Key != ebiten.Key(-1) {
-					isPressed = ebiten.IsKeyPressed(config.Key)
-					// If the main key is pressed, check for modifiers.
-					if isPressed && len(config.Modifiers) > 0 {
-						for _, mod := range config.Modifiers {
-							if !ebiten.IsKeyPressed(mod) {
-								isPressed = false
-								break
-							}
-						}
+		// Now, check for the current state for all bindings.
+		for action := range comp.bindings {
+			for _, binding := range comp.bindings[action] {
+				modsDown := true
+				for _, mod := range binding.Modifiers {
+					if !isPressed(mod) {
+						modsDown = false
+						break
 					}
 				}
 
-				// Check for gamepad input if a button is defined.
-				if config.GamepadButton != ebiten.GamepadButton(-1) {
-					// We assume the first detected gamepad is the one being used.
-					// A more advanced system could handle multiple gamepads.
-					for _, gID := range ebiten.AppendGamepadIDs(nil) {
-						isGamepadButtonDown := ebiten.IsGamepadButtonPressed(gID, config.GamepadButton)
-						// If the main button is pressed, check for modifiers.
-						if isGamepadButtonDown {
-							hasAllModifiers := true
-							for _, mod := range config.GamepadModifiers {
-								if !ebiten.IsGamepadButtonPressed(gID, mod) {
-									hasAllModifiers = false
-									break
-								}
-							}
-							if hasAllModifiers {
-								isPressed = true
-								break // Found a valid gamepad binding, exit this inner loop
-							}
-						}
-					}
+				if isJustPressed(binding.Primary) {
+					comp.justPressed[action] = modsDown
 				}
-
-				// Check for mouse input if a button is defined.
-				if config.MouseButton != ebiten.MouseButton(-1) {
-					isMouseButtonDown := ebiten.IsMouseButtonPressed(config.MouseButton)
-					// If the main mouse button is pressed, check for modifiers.
-					if isMouseButtonDown {
-						hasAllModifiers := true
-						for _, mod := range config.MouseButtonModifiers {
-							if !ebiten.IsMouseButtonPressed(mod) {
-								hasAllModifiers = false
-								break
-							}
-						}
-						if hasAllModifiers {
-							isPressed = true
-							break // Found a valid mouse binding, exit this inner loop
-						}
-					}
+				if isPressed(binding.Primary) {
+					comp.pressed[action] = modsDown
 				}
-
-				// If any binding for this action is pressed, mark the action as active.
-				if isPressed {
-					inputComp.actionState[action] = true
-					break // Stop checking other key configs for this action.
+				if isJustReleased(binding.Primary) {
+					comp.JustReleased[action] = modsDown
 				}
 			}
-
-			// Update the "just pressed" and "just released" states.
-			// An action is "just pressed" if it's currently pressed but was not pressed last frame.
-			// An action is "just released" if it's currently not pressed but was pressed last frame.
-			inputComp.justPressed[action] = inputComp.actionState[action] && !inputComp.previousState[action]
-			inputComp.justReleased[action] = !inputComp.actionState[action] && inputComp.previousState[action]
 		}
 	}
-
 }
